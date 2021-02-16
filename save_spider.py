4785866c0e2d5ef4beed9f336eea39c214606141
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2021-02-15 22:53:44 -0500

    Updating save_spider.py to new pbz2.

--- save_spider.py
+++ save_spider.py
@@ -3,14 +3,14 @@
 from peer_spider import Spider, INTERNAL_NODES
 from pathlib import Path
 from datetime import datetime
-import pickle
+from pickle_util import load_bz2_pickle, save_bz2_pickle
 from network_graph import graph_nodes
 
 SCRIPT_DIR = Path(__file__).parent.absolute()
 DATA_FOLDER = SCRIPT_DIR / "data"
-IPS_FILE = DATA_FOLDER / "ips.pickle"
-NODES_FILE = DATA_FOLDER / f"nodes_{int(datetime.now().timestamp())}.pickle"
-LATEST_FILE = DATA_FOLDER / "nodes_latest.pickle"
+IPS_FILE = DATA_FOLDER / "ips.bpz2"
+NODES_FILE = DATA_FOLDER / f"nodes_{int(datetime.now().timestamp())}.pbz2"
+LATEST_FILE = DATA_FOLDER / "nodes_latest.pbz2"
 GRAPH_FILE = DATA_FOLDER / "graph_latest.png"
 GRAPH_IPS_LATEST = DATA_FOLDER / "ips_latest.csv"
 
@@ -22,26 +22,21 @@ if not DATA_FOLDER.exists():
 def load_ips():
     try:
         if IPS_FILE.exists():
-            with open(IPS_FILE, "rb") as f:
-                return pickle.load(f)
+            return load_bz2_pickle(IPS_FILE)
         return INTERNAL_NODES
-    except pickle.UnpicklingError:
+    except Exception:
         # Should overwrite bad file after generating new ip list
         return INTERNAL_NODES
 
-
-def save_to_pickle(data, filepath):
-    with open(filepath, "wb") as f:
-        pickle.dump(data, f)
 
 ips = load_ips()
 
 spider = Spider(ips)
 spider.get_all_nodes()
 
-save_to_pickle(list(spider.nodes.keys()), IPS_FILE)
-save_to_pickle(spider.nodes, NODES_FILE)
-save_to_pickle(spider.nodes, LATEST_FILE)
+save_bz2_pickle(list(spider.nodes.keys()), IPS_FILE)
+save_bz2_pickle(spider.nodes, NODES_FILE)
+save_bz2_pickle(spider.nodes, LATEST_FILE)
 
 ip_list = graph_nodes(spider.nodes, GRAPH_FILE)
 GRAPH_IPS_LATEST.write_text('\n'.join([f"{ip},{index}" for index, ip in ip_list]))
