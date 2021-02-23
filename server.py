4540434f51d8b57074ddac566ad5da50905a5aed
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2021-02-23 14:20:46 -0500

    Added ip verification to server.py

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, send_file
+from pickle_util import load_bz2_pickle
+from pathlib import Path
 
 app = Flask(__name__)
+SCRIPT_DIR = Path(__file__).parent.absolute()
+DATA_FOLDER = SCRIPT_DIR / "data"
 
 
 @app.route('/')
@@ -11,6 +15,15 @@ def get_image():
 @app.route('/ips')
 def ips():
     return send_file("data/ips_latest.csv", mimetype='text/text')
+
+
+@app.route('/verify/<key>')
+def verify_key(key):
+    VALIDATION_FILE = DATA_FOLDER / "validate_ip.pbz2"
+    ip_dict = load_bz2_pickle(VALIDATION_FILE)
+    if key in ip_dict:
+        return "IP registered."
+    return "IP not registered."
 
 
 if __name__ == '__main__':
