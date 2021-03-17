f305dac7b6f328267eab2021345616f80b2fb590
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2021-03-17 18:06:50 -0400

    Adding upgrade state display to network and moving to a template.

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask, send_file, render_template
 from pickle_util import load_bz2_pickle
 from pathlib import Path
+from collections import defaultdict
 
 app = Flask(__name__)
 SCRIPT_DIR = Path(__file__).parent.absolute()
 DATA_FOLDER = SCRIPT_DIR / "data"
 NODES_LATEST_PATH = DATA_FOLDER / "nodes_latest.pbz2"
+NETWORK_INFO_PATH = DATA_FOLDER / "network_info.pbz2"
 IMAGE_PATH = DATA_FOLDER / "graph_latest.png"
 
 
@@ -23,24 +25,38 @@ def get_image():
 @app.route('/network')
 def network_info():
     # need to replace with template.
-    net_info = load_bz2_pickle("data/network_info.pbz2")
-    output = ["<html><head><title>Delta-11 Network Details</title>",
+    net_info = load_bz2_pickle(NETWORK_INFO_PATH)
+    nodes = load_bz2_pickle(NODES_LATEST_PATH)
+    distinct_upgrades = set()
+    valid_upg = defaultdict(int)
+    weight_pct = defaultdict(int)
+    all_upg = defaultdict(int)
+    node_count = 0
+    val_count = 0
+    for node in nodes.values():
+        node_count += 1
+        if node["next_upgrade"] is None:
+            upgrade = 'None'
+        else:
+            upgrade = node["next_upgrade"]["activation_point"]
+        if node.get("is_validator"):
+            weight = node.get("weight_percent", 0)
+            weight_pct[upgrade] += weight
+            valid_upg[upgrade] += 1
+            val_count += 1
+        all_upg[upgrade] += 1
 
-              "</head><body>",
-              f"<b>Node Count:</b> {net_info['node_count']}",
-              "<p><table border=1><tr><th>Peer Count</th><th>One Way</th><th>Two Way</th></tr>"]
-    for peer_count in net_info["peer_count"]:
-        output.append("<tr>")
-        for i in range(3):
-            output.append(f"<td>{peer_count[i]}</td>")
-        output.append("</tr>")
-    output.append("</table><p>")
-    output.append("<table border=1><tr><th>Path Len</th><th>Count</th></tr>")
-    for path, count in net_info["path_count"]:
-        output.append(f"<tr><td>{path}</td><td>{count}</td></tr>")
-    output.append("</table>")
-    output.append('<img src="./img" width="100%"/></html>')
-    return ''.join(output)
+    upgrade_states = []
+    for key, val in all_upg.items():
+        upgrade_states.append({"state": key,
+                               "all_node_pct": round(val / node_count * 100, 2),
+                               "val_node_pct": round(valid_upg.get(key, 0) / val_count * 100, 2),
+                               "val_wgt_pct": round(weight_pct.get(key, 0), 2)})
+
+    return render_template('network.html',
+                           peer_counts=net_info["peer_count"],
+                           path_counts=net_info["path_count"],
+                           upgrade_states=upgrade_states)
 
 
 if __name__ == '__main__':
