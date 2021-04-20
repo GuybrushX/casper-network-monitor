62310f20c46913d4319420d0df10bf9f1ef70a35
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2021-04-19 21:21:22 -0400

    Adding Make's casper-test as protocol option.

--- protocol.py
+++ protocol.py
@@ -4,14 +4,23 @@ import tarfile
 
 
 PULL_URL = "http://genesis.casperlabs.io"
+TEST_PULL_URL = "https://casper-genesis.make.services"
 CONFIG_ARCHIVE = "config.tar.gz"
 BIN_ARCHIVE = "bin.tar.gz"
+TEST_NETWORK = "casper-test"
+
+
+def pull_url(network: str, protocol: str, archive_name: str) -> str:
+
+    if network == TEST_NETWORK:
+        return f"{TEST_PULL_URL}/{network}/{protocol}/{archive_name}"
+    return f"{PULL_URL}/{network}/{protocol}/{archive_name}"
 
 
 def get_chainspec_config_readme(protocol: str, network: str) -> tuple:
     try:
         with TemporaryDirectory() as temp_dir:
-            r = requests.get(f"{PULL_URL}/{network}/{protocol}/{CONFIG_ARCHIVE}")
+            r = requests.get(pull_url(network, protocol, CONFIG_ARCHIVE))
             archive_path = f"{temp_dir}/{CONFIG_ARCHIVE}"
             with open(archive_path, "wb") as f:
                 f.write(r.content)
@@ -23,7 +32,7 @@ def get_chainspec_config_readme(protocol: str, network: str) -> tuple:
                     if member.name.endswith("config-example.toml"):
                         config_toml = f.read().decode("UTF-8")
 
-            r = requests.get(f"{PULL_URL}/{network}/{protocol}/{BIN_ARCHIVE}")
+            r = requests.get(pull_url(network, protocol, BIN_ARCHIVE))
             archive_path = f"{temp_dir}/{BIN_ARCHIVE}"
             with open(archive_path, "wb") as f:
                 f.write(r.content)
